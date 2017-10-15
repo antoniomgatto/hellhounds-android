defe2da5016aa8b96abc90e128eb99829e0e1808
antoniomgatto <antonio.gatto@gobee.com.br>
2017-10-15 14:11:40 -0200

    install leak canary

--- app/src/main/java/br/com/hellhounds/HellHoundsApplication.java
+++ app/src/main/java/br/com/hellhounds/HellHoundsApplication.java
@@ -5,12 +5,19 @@ import android.app.Application;
 import com.google.firebase.database.FirebaseDatabase;
 import com.orhanobut.logger.AndroidLogAdapter;
 import com.orhanobut.logger.Logger;
+import com.squareup.leakcanary.LeakCanary;
 
 public class HellHoundsApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            return;
+        }
+
+        LeakCanary.install(this);
 
         Logger.addLogAdapter(new AndroidLogAdapter());
 
